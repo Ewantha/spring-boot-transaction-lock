6c66b4c513981d4ad3f78e4a1231082f3fc42cfe
Ewantha <ewantha@outlook.com>
2023-11-07 21:37:48 +0530

    Add retryable methods

--- src/main/java/com/eudagama12/example/transactionlock/service/Transaction.java
+++ src/main/java/com/eudagama12/example/transactionlock/service/Transaction.java
@@ -1,18 +1,17 @@
 package com.eudagama12.example.transactionlock.service;
 
-import java.util.Date;
-
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
 import com.eudagama12.example.transactionlock.entity.Account;
 import com.eudagama12.example.transactionlock.repository.AccountRepository;
-
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.CannotAcquireLockException;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -44,6 +43,7 @@ public class Transaction {
     @Async
     @Transactional
     @SneakyThrows
+    @Retryable(retryFor = CannotAcquireLockException.class)
     public void credit(int id, double amount) {
         log.info("Credit Thread: {}", Thread.currentThread().getName());
         Thread.sleep(1000); // Thread sleep will simulate other operations(e.g. validations) occuring in the transaction.
@@ -60,6 +60,7 @@ public class Transaction {
     @Async
     @Transactional
     @SneakyThrows
+    @Retryable(retryFor = CannotAcquireLockException.class)
     public void debit(int id, double amount) {
         log.info("Debit Thread: {}", Thread.currentThread().getName());
         Thread.sleep(1000); // Thread sleep will simulate other operations(e.g. validations) occuring in the transaction.
