b745acfe0854b048fa6a0fa209e2aefdf6310afb
Ewantha <ewantha@outlook.com>
2023-11-07 21:28:41 +0530

    Add transaction lock

--- src/main/java/com/eudagama12/example/transactionlock/repository/AccountRepository.java
+++ src/main/java/com/eudagama12/example/transactionlock/repository/AccountRepository.java
@@ -1,11 +1,17 @@
 package com.eudagama12.example.transactionlock.repository;
 
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eudagama12.example.transactionlock.entity.Account;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Integer> {
-    
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)    //Add transaction lock
+    Optional<Account> findById(Integer integer);
 }
